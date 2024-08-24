eecf29a3e19ecaf0362f8e444043db3a0ddbd25a
sagyMoveo <sagyb@moveo.co.il>
2024-08-24 17:32:35 +0300

    First connection with server.

--- index.ts
+++ index.ts
@@ -1,12 +1,18 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import { json } from "stream/consumers";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
+
+app.use(cors());
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript!");
+  res.json({ message: "content from server" });
 });
 
 const PORT = process.env.PORT || 3001;
